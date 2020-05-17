7e055aafdba16c19a9c618d09fc78e8d339c8bbc
admorsus <admorsus3@gmail.com>
2020-05-17 15:30:28 +0200

    Add Inner Color Painting (yes/no) option

--- src/mandelbrot/Window.java
+++ src/mandelbrot/Window.java
@@ -7,10 +7,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowStateListener;
 
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -26,7 +29,7 @@ public class Window {
 
 		Container pane = frame.getContentPane();
 		pane.setLayout(new BorderLayout());
-		frame.setLocation(400, 100);
+		frame.setLocation(200, 50);
 
 		display.setDefaultCoords();
 		display.draw();
@@ -79,6 +82,21 @@ public class Window {
 		
 		JComboBox colorSel = new ColorChooser(display).box;
 		botpanel.add(colorSel);
+		
+		JCheckBox innerColorCheck = new JCheckBox("Inner painting");
+		innerColorCheck.addItemListener(new ItemListener() {
+			
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				if (e.getStateChange() == 1) {
+					display.innerColor = 255;
+				} else {
+					display.innerColor = 0;
+				}
+				display.draw();
+			}
+		});
+		botpanel.add(innerColorCheck);
 
 		return botpanel;
 	}
